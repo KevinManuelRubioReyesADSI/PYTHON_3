e8f25002283b61ea584de719b0074e3ec413eab1
KevinManuelRubioReyesADSI <kevinsolo909@gmail.com>
2022-09-30 13:46:52 -0500

    punto_7

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-let avion=20,proyec=10,distacia=0;
-        while(distacia<10000){
-            avion=avion*2;
-            proyec= proyec*2;
-            distacia = avion+proyec;
+let vendedores = document.querySelector("#vendedores").value;
+        let con=1;
+        while(vendedores>=con){
+            let sueldo = parseInt(prompt(`Ingrese el sueldo del vendedor ${con}`));
+            let venta1= parseInt(prompt("Ingrese la venta numero 1"));
+            let venta2= parseInt(prompt("Ingrese la venta numero 2"));
+            let venta3= parseInt(prompt("Ingrese la venta numero 3"));
+            let sueldoTotal = sueldo+ ((venta1+venta2+venta3)*0.10);
+            alert(`El sueldo del vendedor ${con} es de ${sueldoTotal}`);
+            con++;
         }
-        alert(`La distancia ya es mayor de 10000 y la distancia de diferencia es de: ${distacia}`);
