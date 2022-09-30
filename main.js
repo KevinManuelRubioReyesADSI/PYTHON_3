d49899fadd10a45f3d1c283bdc43779da79bfd34
KevinManuelRubioReyesADSI <kevinsolo909@gmail.com>
2022-09-30 13:11:52 -0500

    punto_3

--- main.js
+++ main.js
@@ -1,30 +1,16 @@
-let con=1;
-while(con<=10){
-    num1= parseInt(prompt("Ingrese el numero 1"));
-    num2= parseInt(prompt("Ingrese el numero 2"));
-    let suma1 =suma(num1,num2);
-    let resta1 =resta(num1,num2);
-    let multi1= multi(num1,num2);
-    let divi1 = divi(num1,num2);
-    alert("La suma de los dos numeros es: "+suma1);
-    alert("La resta de los dos numeros es: "+resta1);
-    alert("La multiplicacion de los dos numeros es: "+multi1);
-    alert("La division de los dos numeros es: "+divi1);
-    con++;
-}
-function suma(num1,num2){
-    let res = num1+num2;
-    return res;
-}
-function resta(num1,num2){
-    let res = num1-num2;
-    return res;
-}
-function multi(num1,num2){
-    let res = num1*num2;
-    return res;
-}
-function divi(num1,num2){
-    let res = num1/num2;
-    return res;
-}
+let form = document.querySelector("#form");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let con=1,notaPro=0;
+    while(con<=5){
+        let nombre=prompt(`Ingrese el nombre del estudiante ${con}`);
+        let apellido = prompt(`Ingrese el apellido del estudiante ${con}`);
+        let edad = parseInt(prompt(`Ingrese la edad del estudiante ${con}`));
+        let nota = parseInt(prompt(`Ingrese la nota del estudiante ${con}`));
+        notaPro+=nota;
+        alert(`El nombre del estudiante es ${nombre} y su apellido es ${apellido} tiene una edad de ${edad} y su nota es de ${nota}`);
+        con++;
+    }
+    let pro = notaPro/5;
+    alert("La nota promedio es de: ${pro}");
+    
