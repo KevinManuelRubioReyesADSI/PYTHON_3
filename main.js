cf83b3e7b42061218393a1a527067422e3f7db13
KevinManuelRubioReyesADSI <kevinsolo909@gmail.com>
2022-09-30 13:42:12 -0500

    punto_6

--- main.js
+++ main.js
@@ -1,14 +1,7 @@
-let con=1, aprueban=0,reprueban=0;
-while(con<=40){
-    let nota1=parseInt(prompt(`Ingrese la nota 1 del estudiante ${con}`));
-    let nota2=parseInt(prompt(`Ingrese la nota 2 del estudiante ${con}`));
-    let nota3=parseInt(prompt(`Ingrese la nota 3 del estudiante ${con}`));
-    let nota4=parseInt(prompt(`Ingrese la nota 4 del estudiante ${con}`));
-    let nota5=parseInt(prompt(`Ingrese la nota 5 del estudiante ${con}`));
-    let pro= (nota1+nota2+nota3+nota4+nota5)/5;
-    if (pro<3){
-        reprueban++;
-    } else{
-        aprueban++;
-    } alert(`La cantidad de estudiantes que reprueban son ${reprueban}`);
-    alert(`La cantidad de estudiantes que aprueban son ${aprueban}`);
+let avion=20,proyec=10,distacia=0;
+        while(distacia<10000){
+            avion=avion*2;
+            proyec= proyec*2;
+            distacia = avion+proyec;
+        }
+        alert(`La distancia ya es mayor de 10000 y la distancia de diferencia es de: ${distacia}`);
