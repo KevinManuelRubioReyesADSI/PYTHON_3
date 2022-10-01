f09f1ada7a3aa2d0469e3861f143cdc3b7ab7905
KevinManuelRubioReyesADSI <kevinsolo909@gmail.com>
2022-09-30 19:34:08 -0500

    punto_12

--- main.js
+++ main.js
@@ -1,23 +1,98 @@
-let con=1,amari=0,rosa=0,roja=0,verde=0,azul=0;
-while(vehiculos>=con){
-    let numeroPlaca=parseInt(prompt("Ingrese el numero de la placa"));
-    if (numeroPlaca == 1 || numeroPlaca==2){
-        amari++;
-    } else if (numeroPlaca == 3 || numeroPlaca==4){
-        rosa++;
-    } else if (numeroPlaca == 5 || numeroPlaca==6){
-        roja++;
-    } else if( numeroPlaca ==7 || numeroPlaca ==8){
-        verde++;
-    } else if (numeroPlaca == 9 || numeroPlaca==0){
-        azul++;
-    } else {
-        continue;
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let estudiantes = document.querySelector("#estudiantes").value;
+    let con = 1, a = 0, b = 0, c = 0;
+    while (estudiantes >= con) {
+        let voto = prompt("Ingrese el candidato por el que deseea votar (A, B o C)");
+        if (voto == 'A' || voto == 'a') {
+            a++;
+        } else if (voto == 'B' || voto == 'b') {
+            b++;
+        } else if (voto == 'C' || voto == 'c') {
+            c++;
+        }
+        con++;
     }
-    con++;
-}
-alert(`La cantidad de vehiculos con color amarillo son: ${amari}`);
-alert(`La cantidad de vehiculos con color rosa son: ${rosa}`);
-alert(`La cantidad de vehiculos con color roja son: ${roja}`);
-alert(`La cantidad de vehiculos con color verde son: ${verde}`);
-alert(`La cantidad de vehiculos con color azul son: ${azul}`);
+    let candidatoA = (a / estudiantes) * 100;
+    let candidatoB = (b / estudiantes) * 100;
+    let candidatoC = (c / estudiantes) * 100;
+    let con2 = 1;
+    a=0,b=0,c=0;
+    if (candidatoA > 51) {
+        alert("Gano el candidato A");
+    } else if (candidatoB > 51) {
+        alert("Gano el candidato B");
+    } else if (candidatoC > 51) {
+        alert("Gano el candidato C")
+    } else if (candidatoA==candidatoB && candidatoB==candidatoB){
+        alert("Triple empate");
+    } else if (candidatoA > candidatoB && candidatoB > candidatoC) {
+        let candidatoA=0,candidatoB=0;
+        while (estudiantes >= con2) {
+            let voto = prompt("Ingrese el candidato por el que deseea votar (A o B)");
+            if (voto == 'A' || voto == 'a') {
+                a++;
+            } else if (voto == 'B' || voto == 'b') {
+                b++;
+            }
+            candidatoA = (a / estudiantes) * 100;
+            candidatoB = (b / estudiantes) * 100;
+            con2++
+        }
+        if (candidatoA > candidatoB) {
+            alert("Gano el candidato A!!!");
+        } else if (candidatoB > candidatoA) {
+            alert("Gano el candidato B!!!");
+        } else {
+            alert("Empate");
+        }
+    } else if (candidatoA > candidatoC && candidatoC > candidatoB) {
+        let candidatoA=0,candidatoC=0;
+        while (estudiantes >= con2) {
+            let voto = prompt("Ingrese el candidato por el que deseea votar (A o C)");
+            if (voto == 'A' || voto == 'a') {
+                a++;
+            } else if (voto == 'C' || voto == 'c') {
+                c++;
+            }
+            candidatoA = (a / estudiantes) * 100;
+            candidatoC = (c / estudiantes) * 100;
+            con2++
+        }
+        if (candidatoA > candidatoC) {
+            alert("Gano el candidato A!!!");
+        } else if (candidatoC > candidatoA) {
+            alert("Gano el candidato C!!!");
+        } else {
+            alert("Empate");
+        }
+    } else if (candidatoB > candidatoC && candidatoC > candidatoA) {
+        let candidatoB=0,candidatoC=0;
+        while (estudiantes >= con2) {
+            let voto = prompt("Ingrese el candidato por el que deseea votar (B o C)");
+            if (voto == 'C' || voto == 'c') {
+                c++;
+            } else if (voto == 'B' || voto == 'b') {
+                b++;
+            }
+            candidatoC = (c / estudiantes) * 100;
+            candidatoB = (b / estudiantes) * 100;
+            con2++
+        }
+        if (candidatoB > candidatoC) {
+            alert("Gano el candidato B!!!");
+        } else if (candidatoC > candidatoB) {
+            alert("Gano el candidato C!!!");
+        } else {
+            alert("Empate");
+        }
+    } else if (candidatoB > candidatoA && candidatoA > candidatoC) {
+        let candidatoA=0,candidatoB=0;
+        while (estudiantes >= con2) {
+            let voto = prompt("Ingrese el candidato por el que deseea votar (A o B)");
+            if (voto == 'A' || voto == 'a') {
+                a++;
+            } else if (voto == 'B' || voto == 'b') {
+                b++;
+
+            }
